f23418516d5a608e79eb0c3c649ebc4c145651ed
nourthabet2002 <nour2OO2thabet@gmail.com>
2025-11-18 23:21:59 +0100

    Initial commit

---
+++ mcp-server/src/test/java/ma/enset/mcpserver/McpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.mcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/main/java/ma/enset/ensetbot/EnsetBotApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.ensetbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsetBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnsetBotApplication.class, args);
+    }
+
+}
